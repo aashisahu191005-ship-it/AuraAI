281907e3ab6e51c9ddc812dba519c680ee0838db
Aashi Sahu <aashisahu191005@gmail.com>
2026-05-28 15:13:17 +0530

    fixed api url

--- upload.js
+++ upload.js
@@ -195,7 +195,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 const response =
                     await fetch(
-                        "http://127.0.0.1:5000/analyze",
+                        "/analyze",
                         {
                             method: "POST",
                             body: formData
