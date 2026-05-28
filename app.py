5d0108acff26dde6254ef1e75faf5a1549fdab23
Aashi Sahu <aashisahu191005@gmail.com>
2026-05-28 15:01:45 +0530

    fixed render port

--- app.py
+++ app.py
@@ -332,5 +332,12 @@ def js_files(filename):
 # =========================================
 
 if __name__ == "__main__":
+    import os
 
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
